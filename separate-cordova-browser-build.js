2193d56eb9f7adeef9ee5e8f975164cb5d101775
okland <omri.klinger@gmail.com>
2015-08-23 11:33:54 +0300

    Added support for import.less Fix build of html to html and css to css.

--- separate-cordova-browser-build.js
+++ separate-cordova-browser-build.js
@@ -1,33 +1,38 @@
-var types = ['js','css', 'less', 'html', 'ng.js', 'ng.html'];
+/**
+ * Don't compile file type specified to specified arch
+ * @param fullType
+ * @param arch
+ */
+var registerSourceHandler = function (fullType, arch) {
+    Plugin.registerSourceHandler(fullType, {
+        isTemplate  : true,
+        archMatching: arch
+    }, function (compileStep) {
+        var compileOptions = {
+            path      : compileStep.inputPath,
+            data      : '',
+            sourcePath: compileStep.inputPath
+        };
+        // If js compile to empty js
+        if (fullType.indexOf('js') >= 0) {
+            compileStep.addJavaScript(compileOptions);
+        } else if (fullType.indexOf('html') >= 0) {
+            // Add section to html compile
+            compileOptions.section = 'body';
+            compileStep.addHtml(compileOptions);
+        } else {
+            compileStep.addStylesheet(compileOptions);
+        }
+    });
+};
+
+var types = ['js', 'ng.js', 'ng.html', 'css', 'less', 'import.less', 'html'];
 for (var i in types) {
+    var type = types[i];
     // On browser if has cordova ending
     // Ignore all files with js css or html ending
-    Plugin.registerSourceHandler('cordova.' + types[i], {
-        isTemplate  : true,
-        archMatching: 'web.browser'
-    }, function (compileStep) {
-        // Don't compile Cordova files to web
-        compileStep.addJavaScript({
-            path      : compileStep.inputPath,
-            data      : '',
-            sourcePath: compileStep.inputPath
-        });
-    });
-
-    // On cordova if has browser ending
-    // Ignore all files with js css or html ending
-    Plugin.registerSourceHandler('browser.' + types[i], {
-        isTemplate  : true,
-        archMatching: 'web.cordova'
-    }, function (compileStep) {
-        // Don't compile Cordova files to web
-        compileStep.addJavaScript({
-            path      : compileStep.inputPath,
-            data      : '',
-            sourcePath: compileStep.inputPath
-        });
-    });
+    registerSourceHandler('cordova.' + type, 'web.browser');
+    registerSourceHandler('browser.' + type, 'web.cordova');
 }
-
 
 
